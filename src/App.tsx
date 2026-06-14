fb9bb79872d3702812a68d4d85d9d47867320f41
JuanM1005 <juan.aguirre5548@alumnos.udg.mx>
2026-06-14 12:14:51 -0600

    feat: useCallback.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
 // import AppContent from './AppContent';
 // import AppContent from './AppContent';
-import FocusInput from './components/useRef/FocusInput';
+// import FocusInput from './components/useRef/FocusInput';
 // import BookReader from './components/useRef/useRef';
-import ShoppingCart from './components/useRef/useMemo';
+// import ShoppingCart from './components/useRef/useMemo';
+import {ContactCard} from './components/useRef/UseCallback';
 
 const App = () => {
-  // return <AppContent />;
+  const contact = {
+    id: 1,
+    name: 'Juan',
+    phone: '3312345678',
+  };
+
+  const handleCall = (phone: string) => {
+    console.log(`Llamando al número ${phone}`);
+  };
+
   return (
     <div className="min-h-svh flex items-center justify-center gap-3">
-      {/* <BookReader /> */}
-      <FocusInput />
-      <ShoppingCart />
+      <ContactCard contact={contact} onCall={handleCall} />
     </div>
   );
 };
